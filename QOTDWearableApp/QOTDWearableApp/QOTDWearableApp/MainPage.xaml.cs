eaa5bc011f9c8d7f5679e1b7b5eeb6e7cfc52b7f
RossAlexanderBrown <rossalexanderbrown1993@gmail.com>
2020-01-17 17:35:21 +0000

    Pushing to test on another PC

--- QOTDWearableApp/QOTDWearableApp/QOTDWearableApp/MainPage.xaml.cs
+++ QOTDWearableApp/QOTDWearableApp/QOTDWearableApp/MainPage.xaml.cs
@@ -19,20 +19,26 @@ namespace QOTDWearableApp
     public partial class MainPage : ContentPage
     {
         readonly HttpClient client = new HttpClient();
-        const string Url = "https://api.quotable.io/random";
+        //const string Url = "https://api.quotable.io/random";
         private ObservableCollection<Quote> quote;
         public MainPage()
         {
             InitializeComponent();
             QOTDLabel.Text = "QOTD";
+            AuthorLabel.Text = "Author";
             GetQuote();
         }
 
         async private void GetQuote()
         {
-            string response = await client.GetStringAsync(Url);
-            List<Quote> quotes = JsonConvert.DeserializeObject<List<Quote>>(response);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("Authorization", $"Token {Environment.GetEnvironmentVariable("QOTD_API_KEY")}");
+            string response = await client.GetStringAsync("http://api.paperquotes.com/apiv1/qod");
+            List <Quote> quotes = JsonConvert.DeserializeObject<List<Quote>>(response);
             quote = new ObservableCollection<Quote>(quotes);
+            QOTDLabel.Text = quote.First().content;
+            AuthorLabel.Text = quote.First().author;
         }
     }
 }
