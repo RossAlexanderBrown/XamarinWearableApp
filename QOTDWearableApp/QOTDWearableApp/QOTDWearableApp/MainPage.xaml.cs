127125505d47b299efde334a14a77cbcd3e7cb91
RossAlexanderBrown <rossalexanderbrown1993@gmail.com>
2020-01-17 16:47:28 +0000

    Pushing work at EOD Friday

---
+++ QOTDWearableApp/QOTDWearableApp/QOTDWearableApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using QOTDWearableApp.Models;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
+namespace QOTDWearableApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        readonly HttpClient client = new HttpClient();
+        const string Url = "https://api.quotable.io/random";
+        private ObservableCollection<Quote> quote;
+        public MainPage()
+        {
+            InitializeComponent();
+            QOTDLabel.Text = "QOTD";
+            GetQuote();
+        }
+
+        async private void GetQuote()
+        {
+            string response = await client.GetStringAsync(Url);
+            List<Quote> quotes = JsonConvert.DeserializeObject<List<Quote>>(response);
+            quote = new ObservableCollection<Quote>(quotes);
+        }
+    }
+}
---
+++ QOTDWearableApp/QOTDWearableApp/QOTDWearableApp/Models/Quote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QOTDWearableApp.Models
+{
+    public class Quote
+    {
+        public string _id { get; set; }
+        public string content { get; set; }
+        public string author { get; set; }
+    }
+}
